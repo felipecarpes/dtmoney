c5f23d4bc173a4e04d35328087a1971eb0350f8e
Felipe Augusto Carpes <fcarpes@imply.com>
2022-03-30 00:32:15 -0300

    removing transactions get request from transactions table component

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useTransactions } from "../../hooks/useTransactions";
 import { Container } from "./styles";
 
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    created_at: string;
-}
-
 export function TransactionsTable() {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    const { transactions } = useTransactions();
 
-    useEffect(() => {
-        api.get("transactions")
-        .then(response => setTransactions(response.data.transactions));
-    }, []);
     return (
         <Container>
             <table>
